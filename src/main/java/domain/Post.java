fefae5edf08b5af42ade257c7b9a34d283890087
Alexsandr Orlov <zipoz1@rambler.ru>
2021-01-29 15:49:11 +0300

    initial commit

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,69 @@
+package domain;
+
+public class Post {
+    private int id;
+    private int ownerId;
+    private int date;
+    private String text;
+    private LikesInfo likesInfo;
+    private ShareInfo shareInfo;
+    private CommentsInfo commentsInfo;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LikesInfo getLikesInfo() {
+        return likesInfo;
+    }
+
+    public void setLikesInfo(LikesInfo likesInfo) {
+        this.likesInfo = likesInfo;
+    }
+
+    public ShareInfo getShareInfo() {
+        return shareInfo;
+    }
+
+    public void setShareInfo(ShareInfo shareInfo) {
+        this.shareInfo = shareInfo;
+    }
+
+    public CommentsInfo getCommentsInfo() {
+        return commentsInfo;
+    }
+
+    public void setCommentsInfo(CommentsInfo commentsInfo) {
+        this.commentsInfo = commentsInfo;
+    }
+}
+
---
+++ src/main/java/domain/ShareInfo.java
@@ -0,0 +1,22 @@
+package domain;
+
+public class ShareInfo {
+    private int count;
+    private boolean userShare;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public boolean isUserShare() {
+        return userShare;
+    }
+
+    public void setUserShare(boolean userShare) {
+        this.userShare = userShare;
+    }
+}
